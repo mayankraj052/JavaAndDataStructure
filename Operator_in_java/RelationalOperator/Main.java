48f0bbf714f2497bdc0e13dd38bb094cf2a60a80
Mayank Raj <mayankraj052@gmail.com>
2024-07-26 14:29:34 +0530

    Relational Operator commit

---
+++ Operator_in_java/RelationalOperator/Main.java
@@ -0,0 +1,34 @@
+package Operator_in_java.RelationalOperator;
+
+public class Main {
+    public static void main(String[] args) {
+        // int a = 10, b = 5;
+        // System.out.println(a == b);// false
+        // System.out.println(a != b); // true
+
+        // int a = 10, b = 10;
+        // System.out.println(a == b);// true
+        // System.out.println(a != b); // false
+
+        // int a = 10, b = 10;
+        // System.out.println(a > b);//false
+        // System.out.println(a < b);//false
+        // System.out.println(a >= b);// true
+        // System.out.println(a <= b); //true
+
+        int a = 10, b = 1;
+        System.out.println(a > b); // true
+        System.out.println(a < b);// false
+        System.out.println(a >= b);// true
+        System.out.println(a <= b);// false
+
+    }
+}
+/*
+ * == equality
+ * != equality
+ * > comprasuion
+ * < comprasuion
+ * >= comprasuion
+ * <= comprasuion
+ */
