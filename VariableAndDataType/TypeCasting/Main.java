10d493963556183342690ce2ded812f71604cef5
Mayank Raj <mayankraj052@gmail.com>
2024-07-26 13:14:25 +0530

    Type Casting

---
+++ VariableAndDataType/TypeCasting/Main.java
@@ -0,0 +1,20 @@
+package VariableAndDataType.TypeCasting;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // float b = 5.67777f;
+        // int a = (int) b;
+        // short c = 196;
+        // byte d = (byte) c;
+        // System.out.println(a);// 5
+        // System.out.println(d);
+
+        char ch1 = 'A'; // a -> = 97
+        int num1 = (int) ch1;
+        System.out.println(num1);// 65
+        sc.close();
+    }
+}
