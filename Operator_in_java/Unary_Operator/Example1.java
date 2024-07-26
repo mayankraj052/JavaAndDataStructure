26ce45243fd0b59cb24f221bd8d4cab3cc62065f
Mayank Raj <mayankraj052@gmail.com>
2024-07-26 14:30:07 +0530

    Post and pre increment and decrement

--- Operator_in_java/Unary_Operator/Example1.java
+++ Operator_in_java/Unary_Operator/Example1.java
@@ -4,10 +4,21 @@ package Operator_in_java.Unary_Operator;
 public class Example1 {
     public static void main(String[] args) {
         int x = 10;
-        x++;
-        System.out.println(x);
+        x++; // post increment
+        System.out.println(x); // 11
         int y = 10;
-        y--;
-        System.out.println(y);
+        y--;// post increment
+        System.out.println(y);// 9
+        int a = 10;
+        int b = a--;
+        int c = --a;
+        System.out.println(a);// 8
+        System.out.println(b);// 10
+        System.out.println(c); // 8
     }
 }
+
+// i++ ->first use value then change it.
+// ++i -> first change value then use it.
+// i++ -> post
+// ++ i -> pre
