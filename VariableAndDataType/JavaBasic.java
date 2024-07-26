53418b16689a6fe0abff1da20cd37d9a03848826
Mayank Raj <mayankraj052@gmail.com>
2024-07-26 11:10:21 +0530

    int  type

---
+++ VariableAndDataType/JavaBasic.java
@@ -0,0 +1,14 @@
+package VariableAndDataType;
+
+public class JavaBasic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 40;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "mayank";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+    }
+}
