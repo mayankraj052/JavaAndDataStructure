6daca5af1c10d4df9902481a952325fd7d4d7243
Mayank Raj <mayankraj052@gmail.com>
2024-07-26 14:29:14 +0530

    binary Example commit

--- Operator_in_java/BinaryOperator/Example1.java
+++ Operator_in_java/BinaryOperator/Example1.java
@@ -4,10 +4,11 @@ public class Example1 {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        System.out.println(a + b);
+        System.out.println("Sum = " + a + b);
         System.out.println(a - b);
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(a % b);
     }
 }
+// binary due to two minimum number of operant
