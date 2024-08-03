8ae9995ad567405ed5ae41d25d2d4f25007786d9
Mayank Raj <mayankraj052@gmail.com>
2024-08-03 16:04:35 +0530

    minnor change

---
+++ javaArrays/MultiDimension.java
@@ -0,0 +1,43 @@
+package javaArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        // int[][] arr = {
+        // { 1, 2, 3 },
+        // { 4, 5, 6 },
+        // { 7, 8, 9 }
+        // };
+        // inut
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // System.out.println(arr[i].length); /-> this will give the value of colum in
+        // matrix.
+        // another way for getting output
+        // for (int[] i : arr) {
+        // System.out.println(Arrays.toString(i));
+        // }
+        //
+        // another way for getting output
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+        sc.close();
+    }
+}
