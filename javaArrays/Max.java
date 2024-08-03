12858c46ed868228d34e14c18c6ed7d04030f13b
Mayank Raj <mayankraj052@gmail.com>
2024-08-03 16:33:28 +0530

    finding max array in arraylist

---
+++ javaArrays/Max.java
@@ -0,0 +1,26 @@
+package javaArrays;
+
+public class Max {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5};
+        System.out.println(maxRange(arr,1));
+    }
+
+    static int maxRange(int[] arr, int start ){
+        int end = arr.length - 1;
+        if(start > end){
+            return -1;
+        }
+        if(arr == null){
+            return -1;
+        }
+
+        int maxval = arr[start];
+        for(int i = start; i <= end; i++){
+            if(arr[i] > maxval){
+                maxval = arr[i];
+            }
+        }
+        return maxval;
+    }
+}
