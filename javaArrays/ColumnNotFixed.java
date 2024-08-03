bbdc0c8f23e5e59909744ea286191838e8b25656
Mayank Raj <mayankraj052@gmail.com>
2024-08-03 15:53:02 +0530

    how to acess row and column of 2d array or matrix

---
+++ javaArrays/ColumnNotFixed.java
@@ -0,0 +1,20 @@
+package javaArrays;
+
+public class ColumnNotFixed {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5 },
+                { 6, 7, 8, 9, 88 }
+        };
+
+        // printing
+        for (int i = 0; i < arr.length; i++) { // to acces rows
+            for (int j = 0; j < arr[i].length; j++) { // to acess columns.
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// j<arr[i].length -> array i ka length
