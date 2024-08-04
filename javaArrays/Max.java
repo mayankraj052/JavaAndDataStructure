0fef02baaf0907eb49eae19e33ddee41cc495f3f
Mayank Raj <mayankraj052@gmail.com>
2024-08-04 18:38:37 +0530

    minnor change

--- javaArrays/Max.java
+++ javaArrays/Max.java
@@ -11,10 +11,9 @@ public class Max {
         if(start > end){
             return -1;
         }
-        if(arr == null){
-            return -1;
-        }
-
+        // if(arr == null){
+        //     return -1;
+        // }
         int maxval = arr[start];
         for(int i = start; i <= end; i++){
             if(arr[i] > maxval){
