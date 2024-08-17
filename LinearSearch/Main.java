13215e0dec82ba0cbbebc80a87c4fe5d26c6ad9b
Mayank Raj <mayankraj052@gmail.com>
2024-08-17 09:55:23 +0530

    linear search in 1d array

---
+++ LinearSearch/Main.java
@@ -0,0 +1,34 @@
+package LinearSearch;
+
+// import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] num={23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int target = 45;
+        int ans = linearSearch(num, target);;
+        System.out.println(ans);
+    }
+    // public static boolean linearSearch(int[] arr, int target) {
+    //     if (arr.length == 0) {
+    //         return false;
+    //     }
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] == target) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    public static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
