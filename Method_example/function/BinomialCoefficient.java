549890a1cdd6af308fed9caff1ad4d70cee4b523
Mayank Raj <mayankraj052@gmail.com>
2024-09-03 21:45:23 +0530

    bionomial coefficient

---
+++ Method_example/function/BinomialCoefficient.java
@@ -0,0 +1,24 @@
+package Method_example.function;
+
+public class BinomialCoefficient {
+    public static void main(String[] args) {
+        System.out.println("factorial= " + binomial(5, 2));
+    }
+
+    static int binomial(int n, int r) {
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int n_r_fact = factorial(n - r);
+        int ans = n_fact / (r_fact * n_r_fact);
+        return ans;
+    }
+
+    static int factorial(int n) {
+        int fact = 1;
+        while (n != 0) {
+            fact = fact * n;
+            n--;
+        }
+        return fact;
+    }
+}
