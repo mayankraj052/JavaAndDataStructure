3302a77788be87b3ea1e02965634282c5faa077d
Mayank Raj <mayankraj052@gmail.com>
2024-09-03 23:47:35 +0530

    Java Maths inbuilt method

---
+++ Method_example/function/overloading/JavaMaths.java
@@ -0,0 +1,12 @@
+package Method_example.function.overloading;
+
+public class JavaMaths {
+    public static void main(String[] args) {
+        // int arr[] = { 5, 3, 8, 1, 8 };
+        System.out.println(Math.min(8, 9));
+        System.out.println(Math.max(8, 9));
+        System.out.println(Math.sqrt(625)); // result in float
+        System.out.println(Math.pow(5, 4));// result in float.
+        System.out.println(Math.abs(-700.99)); // provide posotive always
+    }
+}
