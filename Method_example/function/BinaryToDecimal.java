50697a4fdb17e02a79fab3269b17490abe58700e
Mayank Raj <mayankraj052@gmail.com>
2024-09-03 23:08:26 +0530

    binary to dec

---
+++ Method_example/function/BinaryToDecimal.java
@@ -0,0 +1,20 @@
+package Method_example.function;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        binToDec(1010001);
+    }
+
+    public static void binToDec(int binNum) {
+        int power = 0;
+        int decNum = 0;
+        while (binNum > 0) {
+            int ld = binNum % 10;
+            decNum = decNum + (ld * (int) Math.pow(2, power));
+            power++;
+            binNum = binNum / 10;
+        }
+        System.out.println(decNum);
+    }
+}
+
