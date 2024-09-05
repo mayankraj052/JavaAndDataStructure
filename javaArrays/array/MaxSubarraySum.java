2e6ea800603878c74abf5c936d84dd705151f689
Mayank Raj <mayankraj052@gmail.com>
2024-09-05 22:46:01 +0530

    max sum sub array brute force approach

---
+++ javaArrays/array/MaxSubarraySum.java
@@ -0,0 +1,29 @@
+package javaArrays.array;
+
+public class MaxSubarraySum {
+    public static void main(String[] args) {
+        int[] arr = { 1, -2, 6, -1, 3 };
+        maxSubArray(arr);
+    }
+
+    public static void maxSubArray(int arr[]) {
+        int currSum;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                System.out.print("{");
+                currSum = 0;
+                for (int k = i; k <= j; k++) {
+                    currSum += arr[k];
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.print("}");
+                if (currSum > maxSum) {
+                    maxSum = currSum;
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("maxSum= " + maxSum);
+    }
+}
