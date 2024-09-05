31397b91afdee27119441fe68bc9514916d580c5
Mayank Raj <mayankraj052@gmail.com>
2024-09-05 22:06:28 +0530

    sub array

--- javaArrays/array/PrintSubArray.java
+++ javaArrays/array/PrintSubArray.java
@@ -8,11 +8,11 @@ public class PrintSubArray {
 
     public static void subArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            int start = i;
-            for (int j = i + 1; j <= arr.length; j++) {
-                int end = j;
+            // int start = i;
+            for (int j = i; j < arr.length; j++) {
+                // int end = j;
                 System.out.print("{");
-                for (int k = start; k < end; k++) {
+                for (int k = i; k <= j; k++) {
                     System.out.print(arr[k] + " ");
                 }
                 System.out.print("}");
