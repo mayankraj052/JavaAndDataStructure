b582d473f540310cb51f998d2530ffe99905b02c
Mayank Raj <mayankraj052@gmail.com>
2024-09-05 21:44:10 +0530

    pair of element in arra

---
+++ javaArrays/array/PairInArray.java
@@ -0,0 +1,17 @@
+package javaArrays.array;
+
+public class PairInArray {
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 7, 8 };
+        pairArray(arr);
+    }
+
+    public static void pairArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + curr + "," + arr[j] + ") ");
+            }
+        }
+    }
+}
