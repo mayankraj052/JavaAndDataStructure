a2f144b9799c27bcf9ac068d28e026300ba3d445
Mayank Raj <mayankraj052@gmail.com>
2024-09-07 14:17:18 +0530

    Search in Rotated Sorted Array

---
+++ javaArrays/array/SearchRoatedSorted.java
@@ -0,0 +1,44 @@
+package javaArrays.array;
+
+public class SearchRoatedSorted {
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int target1 = 0;
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        int target2 = 3;
+        System.out.println("Index of target " + target1 + ": " + search(arr, target1));
+        System.out.println("Index of target " + target2 + ": " + search(nums, target2));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Determine which half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    // Target is in the left half
+                    right = mid - 1;
+                } else {
+                    // Target is in the right half
+                    left = mid + 1;
+                }
+            } else {
+                // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    // Target is in the right half
+                    left = mid + 1;
+                } else {
+                    // Target is in the left half
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
