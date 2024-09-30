694628b298286e025b5e3aa0caa78df94171a007
Mayank Raj <mayankraj052@gmail.com>
2024-09-30 21:50:21 +0530

    selection sort

--- Basic_Sorting/SelectionSort.java
+++ Basic_Sorting/SelectionSort.java
@@ -2,6 +2,24 @@ package Basic_Sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        
+        int[] data = { 20, 12, 10, 15, 2 };
+        selectionSort(data);
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void selectionSort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[min] > data[j]) {
+                    min = j;
+                }
+            }
+            int temp = data[min];
+            data[min] = data[i];
+            data[i] = temp;
+        }
     }
 }
